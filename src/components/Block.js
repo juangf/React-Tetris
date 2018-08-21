e2f8c197dea56e84c03375bfc4d1b6d9f7b3a9f2
Juan García <jgarcia@bodas.net>
2018-08-21 20:13:59 +0200

    chore: Changed project structure

---
+++ src/components/Block.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/Block.css';
+
+class Block extends Component {
+	constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Block">
+      </div>
+    );
+  }
+}
+
+export default Block;
