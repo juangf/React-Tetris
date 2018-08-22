b1cda7d3d20fcc837ef6fd6000ef7d6e587a9c59
Juan García <jgarcia@bodas.net>
2018-08-23 00:16:50 +0200

    feat: get pieces method

--- src/components/BlockMatrix.js
+++ src/components/BlockMatrix.js
@@ -3,12 +3,59 @@ import Block from './Block';
 import '../css/BlockMatrix.css';
 
 class BlockMatrix extends Component {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPosition : {
+        x : 0,
+        y : 0
+      },
+      pieces : []
+    };
+
+    this.pieces = this.getPieces();
+  }
+
+  getPieces() {
+    // Pieces origin and rotation point: x = 0, y = 0
+    return [
+      [
+        [-2, 0], [-1, 0], [0, 0], [0, 1]
+      ],
+      [
+        [-1, -1],
+        [-1, 0], [0, 0], [1, 0]
+      ],
+      [
+                         [1, -1],
+        [-1, 0], [0, 0], [1, 0]
+      ],
+      [
+        [0, 0], [1, 0],
+        [0, 1], [1, 1]
+      ],
+      [
+                [0, -1], [1, -1],
+        [-1, 0], [0, 0]
+      ],
+      [
+                 [0, -1],
+        [-1, 0], [0, 0], [1, 0]
+      ],
+      [
+        [-1, -1],[0, -1],
+                 [0, 0], [1, 0]
+      ]
+    ];
+  }
+  
   render() {
     let size = 10 * 22;
     return (
       <div className="BlockMatrix">
         {[...Array(size)].map((x, i) =>
-          <Block />
+          <Block key={i} />
         )}
       </div>
     );
